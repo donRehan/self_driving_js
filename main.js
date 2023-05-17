5f76af6b2ac54aa19198854c7271f9510101b6de
BluBird4 <alhussienrehan@hotmail.com>
2023-05-17 17:40:00 +0200

    fixed bug && Implemented car motion without friction

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 // Road canvas
 const canvas=document.getElementById('myCanvas');
-canvas.height=window.innerHeight;
 canvas.width=200;
 
 // car 
@@ -8,10 +7,13 @@ const ctx = canvas.getContext('2d');
 const car = new Car(100,100,30,50);
 
 //console.log('Car drawn:', this.x, this.y, this.width, this.height);
-
+//car.draw(ctx);
+animate();
+//
 function animate(){
   car.update();
-  car.draw(ctx);
-}
 
-requestAnimatio(animate);
+  canvas.height=window.innerHeight;
+  car.draw(ctx);
+  requestAnimationFrame(animate);
+}
